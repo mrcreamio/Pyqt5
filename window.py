600d0df9946a39830108dabf35142fcf96afee07
ahmed Waheed <aw0reborn@gmail.com>
2019-11-20 00:27:57 +0500

    added extrapolate function

--- window.py
+++ window.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import QInputDialog, QFileDialog, QTableWidgetItem
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
+import numpy as np
+from scipy import stats
 
 class Window:
     def __init__(self):
@@ -10,6 +12,7 @@ class Window:
         self.dlg = uic.loadUi("MainWindow.ui")
         self.dlg.SelectFile.clicked.connect(self.selectFile)
         self.dlg.barchart.clicked.connect(self.BarChart)
+        self.dlg.extrapolate.clicked.connect(self.extrapolate)
         self.dlg.show()
         app.exec()
 
@@ -35,10 +38,10 @@ class Window:
         industry = df['Industrial Sector']
         services = df['Services Sector']
         domestic = df['Gross Domestic Product (FC)']
-        df.describe()
+        # df.describe()
         fig = plt.figure()
         fig.add_subplot(221)
-        plt.bar(agriculture)
+        plt.bar(x,agriculture)
         plt.title("Agriculture")
         fig.add_subplot(222)
         plt.bar(x, industry)
@@ -50,6 +53,58 @@ class Window:
         plt.bar(x, domestic)
         plt.title("domestic")
         plt.show()
+    def extrapolate(self):
+        df = pd.read_csv(self.file)
+        a = df['Agriculture']
+        b = df['Industrial Sector']
+        c = df['Services Sector']
+        d = df['Gross Domestic Product (FC)']
+        x = df['Year']
+        fig = plt.figure()
+        fig.add_subplot(221)
+        plt.plot(x, a, 'ro')
+        z = np.polyfit(x, a, 20)
+        f = np.poly1d(z)
+        x2 = []
+        for x1 in np.linspace(1950, 2020, 70):
+            x2.append(x1)
+        plt.plot(x2, f(x2), 'b+')
+        
+        fig.add_subplot(222)
+    # #---------------------
+        plt.plot(x, b, 'ro')    
+        z = np.polyfit(x, b, 20)
+        f = np.poly1d(z)
+        x2 = []
+        for x1 in np.linspace(1950, 2020, 70):
+            x2.append(x1)
+        plt.plot(x2, f(x2), 'b+')
+        
+        fig.add_subplot(223)
+    # #---------------------
+        plt.plot(x, c, 'ro')
+        z = np.polyfit(x, c, 20)
+        f = np.poly1d(z)
+        x2 = []
+        for x1 in np.linspace(1950, 2020, 70):
+            x2.append(x1)
+        plt.plot(x2, f(x2), 'b+')
+        
+        fig.add_subplot(224)
+    # #---------------------
+        plt.plot(x, d, 'ro')
+        z = np.polyfit(x, d, 20)
+        f = np.poly1d(z)
+        x2 = []
+        for x1 in np.linspace(1950, 2020, 70):
+            x2.append(x1)
+        plt.plot(x2, f(x2), 'b+')
+        
+        plt.show()
+        
+    # t test function
+    # stats.ttest_ind(df['Agriculture'], df['Industrial Sector'])
+
 
 
 window= Window()
