7052f12e2dbd37cb7110c3ca88d3c2c29c9b71e2
ahmed Waheed <aw0reborn@gmail.com>
2019-11-19 17:19:05 +0500

    done

--- cod1.py
+++ cod1.py
@@ -1,14 +1,36 @@
 from PyQt5 import QtWidgets, uic, QtGui
-from PyQt5.QtWidgets import QInputDialog, QFileDialog
+from PyQt5.QtWidgets import QInputDialog, QFileDialog,QTableWidgetItem
+import pyqtgraph as pg
+import csv
+
+
 
 def selectFile():
-    fileName = (str(QFileDialog.getOpenFileName()))
+    fileName = (QFileDialog.getOpenFileName())
     print(fileName)
-    file1 = open(fileName[0],'r')
-    dlg.editor()
-    with file1:
-        text = file1.read()
-        dlg.textBrowser.setText(text)
+
+    with open(fileName[0]) as csvfile:
+        reader = csv.DictReader(csvfile)
+        i = 0
+        for row in reader:
+            # print(row['Agriculture'], row['Industrial Sector'], row['Services Sector'],row['Gross Domestic Product (FC)'])
+                dlg.tableWidget.setRowCount(70)
+                # dlg.tableWidget.setItem(2 , 0, QtGui.QTableWidgetItem("text1"))
+                dlg.tableWidget.setItem(i,0, QTableWidgetItem(row['Agriculture']))
+                dlg.tableWidget.setItem(i,1, QTableWidgetItem(row['Industrial Sector']))
+                dlg.tableWidget.setItem(i,2, QTableWidgetItem(row['Services Sector']))
+                dlg.tableWidget.setItem(i,3, QTableWidgetItem(row['Gross Domestic Product (FC)']))
+                i+=1
+
+
+
+    # file1 = open(fileName[0],'r')
+    # with file1:
+    #     text = file1.read()
+    #     dlg.textBrowser.setText(text)
+def drawgraphs():
+    pass
+
 
 
 app = QtWidgets.QApplication([])
